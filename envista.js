b1323a6e282c48432c65c927a5486613be94037c
Holly Davis <holly@hollycdavis.com>
2016-08-07 22:25:28 -0700

    update

--- envista.js
+++ envista.js
@@ -1,14 +1,6 @@
 var app = angular.module("app", ["leaflet-directive"]);
 
 app.controller('MapController', ['$scope', '$http', 'leafletData', function($scope, $http, leafletData) {
-  var smallIcon = new L.Icon({
-      iconUrl: 'icons/pin-2_dk_blue.svg', // was using 'icons/placeholder-29.svg'
-      iconRetinaUrl: 'icons/pin-2_dk_blue.svg',
-      iconSize:    [34, 34], // size of the icon
-      iconAnchor:  [18, 32], // 12, 30, point of the icon which will correspond to marker's location
-      popupAnchor: [10, -34], // 4, -30, point from which the popup should open relative to the iconAnchor
-  });
-
   var initLat = 37.759313;
   var initLong = -122.441815;
   var initZoomLevel = 12;
@@ -48,16 +40,24 @@ app.controller('MapController', ['$scope', '$http', 'leafletData', function($sco
       var markers = L.markerClusterGroup();
       var geoJsonLayer = L.geoJson(data, {
           pointToLayer: function(feature, latlng) {
+             var smallIcon = new L.Icon({
+                    iconSize:    [34, 34], // size of the icon
+                    iconAnchor:  [18, 32], // 12, 30, point of the icon which will correspond to marker's location
+                    popupAnchor: [10, -34], // 4, -30, point from which the popup should open relative to the iconAnchor
+                    // iconRetinaUrl: 'icons/' + feature.properties.project_status + '.svg',
+                    iconUrl: 'icons/' + feature.properties.project_status + '.svg'
+             });
+             //console.log(smallIcon);
              //console.log(latlng, feature);
              return L.marker(latlng, {
-               icon: smallIcon
+                     icon: smallIcon
              });
            },
            onEachFeature: onEachFeature
       });
       markers.addLayer(geoJsonLayer);
       leafletData.getMap().then(function(map) {
-        map.addLayer(markers);
+          map.addLayer(markers);
       });
   }
 
