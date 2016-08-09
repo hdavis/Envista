30a0888aa26a4c38a34980b67ed082f674a40769
Holly Davis <holly@hollycdavis.com>
2016-08-08 18:50:06 -0700

    update

--- envista.js
+++ envista.js
@@ -43,7 +43,7 @@ app.controller('MapController', ['$scope', '$http', 'leafletData', function($sco
              var smallIcon = new L.Icon({
                     iconSize:    [30, 30], // 34, 34, size of the icon
                     iconAnchor:  [18, 32], // 12, 30, point of the icon which will correspond to marker's location
-                    popupAnchor: [-3, -34], // 4/10, -30, point from which the popup should open relative to the iconAnchor
+                    popupAnchor: [-2, -34], // 4, -30, point from which the popup should open relative to the iconAnchor
                     // iconRetinaUrl: 'icons/' + feature.properties.project_status + '.svg',
                     iconUrl: 'icons/' + feature.properties.project_status + '.svg'
              });
